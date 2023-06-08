4c117234f9a3cd3ce05a027b52c32f85f297c28a
maul49zer@gmail.com <maul49zer@gmail.com>
2023-06-08 13:31:50 -0600

    done

--- src/helpers/film.helpers.js
+++ src/helpers/film.helpers.js
@@ -1,3 +1,5 @@
+// import exp from "constants";
+
 export function filterFilmsByDirector(list, director) {
   if (director) return list.filter((film) => film.director == director);
   else return list;
@@ -5,4 +7,23 @@ export function filterFilmsByDirector(list, director) {
 
 export function getListOf(list, prop) {
   return [...new Set(list.map((film) => film[prop] || ""))];
+}
+
+export function getFilmStats(list) {
+  return list.reduce((stats, film) => {
+    stats.total++;
+    stats.acc_score += Number(film.rt_score);
+    stats.avg_score = stats.acc_score / stats.total;
+
+    if (stats.lastest == null || stats.lastest < film.release_date) {
+      stats.lastest = film.release_date;
+    } 
+
+    return stats;
+  }, {
+    acc_score: 0,
+    avg_score: 0,
+    total: 0,
+    lastest: null
+  });
 }
